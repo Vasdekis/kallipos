074a627c54a6d17c3ae92cede828107544d82456
Sotiris Vasdekis <99913413+Vasdekis@users.noreply.github.com>
2022-11-19 15:03:17 +0200

    Update prosthikes.lua

--- prosthikes.lua
+++ prosthikes.lua
@@ -1,11 +1,12 @@
 function Image(img)
-      if img.classes:find('prosthikes',1) then
-        local f = io.open("quotes/" .. img.src, 'r')
-        local doc = pandoc.read(f:read('*a'))
-        f:close()
-        local caption = pandoc.utils.stringify(doc.meta.caption) or "Epigraph has not been set"
-        local person = pandoc.utils.stringify(doc.meta.person) or "Person has not been set"
-        local prosthikes = "> " .. caption .. " " .. person
-        return pandoc.RawInline('markdown',prosthikes)
-      end
+    if img.classes:find('prosthikes',1) then
+      local f = io.open("comment/" .. img.src, 'r')
+      local doc = pandoc.read(f:read('*a'))
+      f:close()
+      local caption = pandoc.utils.stringify(doc.meta.caption) 
+      local name = pandoc.utils.stringify(doc.meta.name)
+      local am = pandoc.utils.stringify(doc.meta.id)
+      local content = ">_" .. caption .. " _ \n>" .. "Σωτήρης Βασδέκης:" .. name .. "\nΠ2018163:" .. am
+      return pandoc.RawInline('markdown',content)
+    end
 end
